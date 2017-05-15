f2f1813a3df6c2c048ac94016a444310deaeeca5
Jack Cook <hello@jackcook.nyc>
2017-05-15 18:25:10 -0400

    Remove unused imports

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import pandas as pd
 import numpy as np
-import random as rnd
 
 import seaborn as sns
-import matplotlib.pyplot as plt
-
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
 
 train_df = pd.read_csv("./input/train.csv")
 test_df = pd.read_csv("./input/test.csv")
