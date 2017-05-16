cc7ed451ddd625214500aa118fed8e331b440326
Jack Cook <hello@jackcook.nyc>
2017-05-16 08:30:02 -0400

    Further simplified code

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 import pandas as pd
-import numpy as np
-
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 
-train_df = pd.read_csv("./input/train.csv").drop(["Ticket", "Cabin"], axis=1)
+train_df = pd.read_csv("./input/train.csv").drop(["PassengerId", "Ticket", "Cabin"], axis=1)
 test_df = pd.read_csv("./input/test.csv").drop(["Ticket", "Cabin"], axis=1)
 combine = [train_df, test_df]
 
-title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-
 for dataset in combine:
-    dataset["Title"] = dataset.Name.str.extract(" ([A-Za-z]+)\.", expand=False)
-    dataset["Title"] = dataset["Title"].replace(["Lady", "Countess", "Capt", "Col", "Don", "Dr", "Major", "Rev", "Sir", "Jonkheer", "Dona"], "Rare")
-    dataset["Title"] = dataset["Title"].replace("Mlle", "Miss")
-    dataset["Title"] = dataset["Title"].replace("Ms", "Miss")
-    dataset["Title"] = dataset["Title"].replace("Mme", "Mrs")
-    dataset["Title"] = dataset["Title"].map(title_mapping)
-    dataset["Title"] = dataset["Title"].fillna(0)
+    dataset["Title"] = dataset.Name.str.extract(" ([A-Za-z]+)\.", expand=False) \
+        .replace(["Lady", "Countess", "Capt", "Col", "Don", "Dr", "Major", "Rev", "Sir", "Jonkheer", "Dona"], "Rare") \
+        .replace("Mlle", "Miss") \
+        .replace("Ms", "Miss") \
+        .replace("Mme", "Mrs") \
+        .map({"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}) \
+        .fillna(0)
 
-train_df = train_df.drop(["Name", "PassengerId"], axis=1)
+train_df = train_df.drop(["Name"], axis=1)
 test_df = test_df.drop(["Name"], axis=1)
 combine = [train_df, test_df]
-
-guess_ages = np.zeros((2, 3))
 
 for dataset in combine:
     dataset["Sex"] = dataset["Sex"].map({"female": 1, "male": 0}).astype(int)
@@ -31,16 +25,10 @@ for dataset in combine:
     for i in range(0, 2):
         for j in range(0, 3):
             guess_df = dataset[(dataset["Sex"] == i) & (dataset["Pclass"] == j + 1)]["Age"].dropna()
-            age_guess = guess_df.median()
-            guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
-
-    for i in range(0, 2):
-        for j in range(0, 3):
-            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), "Age"] = guess_ages[i, j]
+            age_guess = int(guess_df.median() / 0.5 + 0.5) * 0.5
+            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), "Age"] = age_guess
 
     dataset["Age"] = dataset["Age"].astype(int)
-
-train_df["AgeBand"] = pd.cut(train_df["Age"], 5)
 
 for dataset in combine:
     dataset.loc[dataset["Age"] <= 16, "Age"] = 0
@@ -49,7 +37,6 @@ for dataset in combine:
     dataset.loc[(dataset["Age"] > 48) & (dataset["Age"] <= 64), "Age"] = 3
     dataset.loc[dataset["Age"] > 64, "Age"] = 4
 
-train_df = train_df.drop(["AgeBand"], axis=1)
 combine = [train_df, test_df]
 
 for dataset in combine:
@@ -63,7 +50,6 @@ combine = [train_df, test_df]
 
 freq_port = train_df.Embarked.dropna().mode()[0]
 test_df["Fare"].fillna(test_df["Fare"].dropna().median(), inplace=True)
-train_df["FareBand"] = pd.qcut(train_df["Fare"], 4)
 
 for dataset in combine:
     dataset["Age*Class"] = dataset.Age * dataset.Pclass
@@ -75,8 +61,6 @@ for dataset in combine:
     dataset.loc[(dataset["Fare"] > 14.454) & (dataset["Fare"] <= 31), "Fare"] = 2
     dataset.loc[dataset["Fare"] > 31, "Fare"] = 3
     dataset["Fare"] = dataset["Fare"].astype(int)
-
-train_df = train_df.drop(["FareBand"], axis=1)
 
 X_train = train_df.drop("Survived", axis=1)
 Y_train = train_df["Survived"]
