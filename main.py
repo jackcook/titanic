02ad9b0d6dbcc5a39d38d8645936318f9e5157b6
Jack Cook <hello@jackcook.nyc>
2017-05-18 08:46:38 -0400

    Created a method for cleaning a dataframe

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 
-train_df = pd.read_csv("./input/train.csv").drop(["PassengerId", "Ticket", "Cabin"], axis=1)
-test_df = pd.read_csv("./input/test.csv").drop(["Ticket", "Cabin"], axis=1)
-combine = [train_df, test_df]
-
-for dataset in combine:
+def clean(dataset):
     dataset["Title"] = dataset.Name.str.extract(" ([A-Za-z]+)\.", expand=False) \
         .replace(["Lady", "Countess", "Capt", "Col", "Don", "Dr", "Major", "Rev", "Sir", "Jonkheer", "Dona"], "Rare") \
         .replace("Mlle", "Miss") \
@@ -15,11 +11,6 @@ for dataset in combine:
         .map({"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}) \
         .fillna(0)
 
-train_df = train_df.drop(["Name"], axis=1)
-test_df = test_df.drop(["Name"], axis=1)
-combine = [train_df, test_df]
-
-for dataset in combine:
     dataset["Sex"] = dataset["Sex"].map({"female": 1, "male": 0}).astype(int)
 
     for i in range(0, 2):
@@ -30,36 +21,31 @@ for dataset in combine:
 
     dataset["Age"] = dataset["Age"].astype(int)
 
-for dataset in combine:
     dataset.loc[dataset["Age"] <= 16, "Age"] = 0
     dataset.loc[(dataset["Age"] > 16) & (dataset["Age"] <= 32), "Age"] = 1
     dataset.loc[(dataset["Age"] > 32) & (dataset["Age"] <= 48), "Age"] = 2
     dataset.loc[(dataset["Age"] > 48) & (dataset["Age"] <= 64), "Age"] = 3
     dataset.loc[dataset["Age"] > 64, "Age"] = 4
 
-combine = [train_df, test_df]
-
-for dataset in combine:
     dataset["FamilySize"] = dataset["SibSp"] + dataset["Parch"] + 1
     dataset["IsAlone"] = 0
     dataset.loc[dataset["FamilySize"] == 1, "IsAlone"] = 1
 
-train_df = train_df.drop(["Parch", "SibSp", "FamilySize"], axis=1)
-test_df = test_df.drop(["Parch", "SibSp", "FamilySize"], axis=1)
-combine = [train_df, test_df]
-
-freq_port = train_df.Embarked.dropna().mode()[0]
-test_df["Fare"].fillna(test_df["Fare"].dropna().median(), inplace=True)
-
-for dataset in combine:
+    freq_port = dataset.Embarked.dropna().mode()[0]
     dataset["Embarked"] = dataset["Embarked"].fillna(freq_port)
     dataset["Embarked"] = dataset["Embarked"].map({"S": 0, "C": 1, "Q": 2}).astype(int)
 
+    dataset["Fare"].fillna(dataset["Fare"].dropna().median(), inplace=True)
     dataset.loc[dataset["Fare"] <= 7.91, "Fare"] = 0
     dataset.loc[(dataset["Fare"] > 7.91) & (dataset["Fare"] <= 14.454), "Fare"] = 1
     dataset.loc[(dataset["Fare"] > 14.454) & (dataset["Fare"] <= 31), "Fare"] = 2
     dataset.loc[dataset["Fare"] > 31, "Fare"] = 3
     dataset["Fare"] = dataset["Fare"].astype(int)
+
+    return dataset.drop(["Name", "Parch", "SibSp", "FamilySize"], axis=1)
+
+train_df = clean(pd.read_csv("./input/train.csv").drop(["PassengerId", "Ticket", "Cabin"], axis=1))
+test_df = clean(pd.read_csv("./input/test.csv").drop(["Ticket", "Cabin"], axis=1))
 
 X_train = train_df.drop("Survived", axis=1)
 Y_train = train_df["Survived"]
